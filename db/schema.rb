13a37646891d370525fa38646fcf5eddc3f731e1
JenJen <jennifer7793@gmail.com>
2021-12-23 12:10:24 +0800

    feat:setup devise

--- db/schema.rb
+++ db/schema.rb
@@ -63,6 +63,16 @@ ActiveRecord::Schema.define(version: 2021_12_23_035350) do
     t.string "blind_carbon_copy"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
