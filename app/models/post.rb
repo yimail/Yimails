49bd2ef7cc48f145ba6469c9978002280f5464cc
elik <akainooo@gmail.com>
2021-12-23 17:57:52 +0800

    feat:add_mailbox

---
+++ app/models/post.rb
@@ -0,0 +1,5 @@
+class Post < ApplicationRecord
+  has_rich_text :body
+  has_many_attached :attachments
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -9,8 +9,7 @@
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
-
-ActiveRecord::Schema.define(version: 2021_12_23_041450) do
+ActiveRecord::Schema.define(version: 2021_12_23_084941) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -91,6 +90,12 @@ ActiveRecord::Schema.define(version: 2021_12_23_041450) do
     t.datetime "last_sign_in_with_authy"
     t.boolean "authy_enabled", default: false
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
