93ad4f16e8afcabfa586f5d55f8060cb4237e9ee
danachen <danac.tw@gmail.com>
2021-12-27 15:25:01 +0800

    feat:install authy

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_many :letters
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :authy_authenticatable, :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,  :authentication_keys => [:username]
 
   validates :firstname, :lastname, :username, :backup_email, presence: true
