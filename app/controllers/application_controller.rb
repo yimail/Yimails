b379b82d29401ce3378b6189888842fb7526c9c8
JenJen <jennifer7793@gmail.com>
2021-12-29 21:22:41 +0800

    set up i18n for lang switching to en (default:zh-TW)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 class ApplicationController < ActionController::Base
+  def default_url_options
+    { locale: I18n.locale }
+  end
+  
+  around_action :switch_locale
+
+  def switch_locale(&action)
+    locale = params[:locale] || I18n.default_locale
+    I18n.with_locale(locale, &action)
+  end
+
   def after_sign_in_path_for(resource)
     letters_path(current_user)
   end
