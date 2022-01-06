e9d08f866d8ef8225242b03a46e757fb3274867d
Elaine Wu <amsmm265@gmail.com>
2022-01-06 10:46:32 +0800

    fix

--- app/service/newebpay/mpg.rb
+++ app/service/newebpay/mpg.rb
@@ -30,7 +30,7 @@ module Newebpay
       info[:TimeStamp] = Time.now.to_i.to_s
       info[:RespondType] = "JSON"
       info[:Version] = "1.5"
-      info[:ReturnURL] = 'http://localhost:3000/payment_response'
+      info[:ReturnURL] = 'https://yimails.com/payment_response'
       info[:NotifyURL] = ''
       info[:LoginType] = 0
       info[:CREDIT] = 1
