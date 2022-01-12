29d4717427153057e04cf47904daea2d12090990
danachen <danac.tw@gmail.com>
2022-01-12 09:36:05 +0800

    fix:關聯表名字改複數

--- app/models/label.rb
+++ app/models/label.rb
@@ -1,6 +1,6 @@
 class Label < ApplicationRecord
   belongs_to :user
 
-  has_many :letter_with_label
-  has_many :letters, through: :letter_with_label
+  has_many :letter_with_labels
+  has_many :letters, through: :letter_with_labels
 end
