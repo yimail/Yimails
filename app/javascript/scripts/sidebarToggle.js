54bf7e9f35ec7a68a1a2c535b81befd8c5cf1658
Erik <jjjay5223758@gmai.com>
2022-01-16 23:01:17 +0800

    fix:search bar RWD

--- app/javascript/scripts/sidebarToggle.js
+++ app/javascript/scripts/sidebarToggle.js
@@ -1,10 +1,9 @@
 document.addEventListener('turbolinks:load', () => {
-  const btn = document.querySelector('#sidebarBtn');
+  var btn = document.querySelector('#sidebarBtn');
   const sidebar = document.querySelector('#sidebar');
   const sidebarBackdrop = document.querySelector('#sidebarBackdrop');
 
   btn.addEventListener("click", () => {
-    console.log(sidebar.classList)
   sidebar.classList.toggle('hidden');
   sidebarBackdrop.classList.toggle('hidden');
   })
