8bb302fa210350b34ba18355cdd91478033871ec
Erik <jjjay5223758@gmai.com>
2022-01-17 22:39:10 +0800

    fix:show排版

--- app/javascript/scripts/sidebarToggle.js
+++ app/javascript/scripts/sidebarToggle.js
@@ -1,10 +1,10 @@
 document.addEventListener('turbolinks:load', () => {
-  var btn = document.querySelector('#sidebarBtn');
+  const btn = document.querySelector('#sidebarBtn');
   const sidebar = document.querySelector('#sidebar');
   const sidebarBackdrop = document.querySelector('#sidebarBackdrop');
-
+  
   btn.addEventListener("click", () => {
-  sidebar.classList.toggle('hidden');
-  sidebarBackdrop.classList.toggle('hidden');
+    sidebar.classList.toggle('hidden');
+    sidebarBackdrop.classList.toggle('hidden');
   })
 })
