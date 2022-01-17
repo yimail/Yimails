5f3fbbd169be13a1c1486e7f77a8bc77bf384f5d
Elaine Wu <amsmm265@gmail.com>
2022-01-17 10:03:53 +0800

    fix:搜尋及標籤頁面

--- app/controllers/letters_controller.rb
+++ app/controllers/letters_controller.rb
@@ -73,7 +73,7 @@ class LettersController < ApplicationController
   end
 
   def search
-    @letters = current_user.letters.where("sender ILIKE ? or recipient ILIKE ? or subject ILIKE ?", "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%").order(id: :desc)
+    @letters = current_user.letters.where("sender ILIKE ? or recipient ILIKE ? or subject ILIKE ?", "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%").order(id: :desc).page(params[:page]).per(25)
   end
 
   private
