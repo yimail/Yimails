318c4e28356be6e2c2d5d35fa2bede68d2ddca8b
Erik <jjjay5223758@gmai.com>
2022-01-18 23:31:02 +0800

    feat:email 驗證

--- app/javascript/scripts/sidebarToggle.js
+++ app/javascript/scripts/sidebarToggle.js
@@ -1,10 +1,10 @@
 document.addEventListener('turbolinks:load', () => {
-  const btn = document.querySelector('#sidebarBtn');
+  var btn = document.querySelector('#sidebarBtn');
   const sidebar = document.querySelector('#sidebar');
   const sidebarBackdrop = document.querySelector('#sidebarBackdrop');
-  
+
   btn.addEventListener("click", () => {
-    sidebar.classList.toggle('hidden');
-    sidebarBackdrop.classList.toggle('hidden');
+  sidebar.classList.toggle('hidden');
+  sidebarBackdrop.classList.toggle('hidden');
   })
 })
